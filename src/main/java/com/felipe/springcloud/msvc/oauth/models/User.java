7b46d3e246945b4ed83d97851383894ad282127b
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-05-11 14:50:44 -0400

    Add User and Role models; implement UserService to load user details with roles

---
+++ src/main/java/com/felipe/springcloud/msvc/oauth/models/User.java
@@ -0,0 +1,79 @@
+package com.felipe.springcloud.msvc.oauth.models;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private Boolean enabled;
+    private String email;
+    private List<Role> roles;
+
+    private boolean admin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isAdmin() {
+        return this.admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
+                + ", email=" + email + ", roles=" + roles + ", admin=" + admin + "]";
+    }
+
+    // Getters and Setters
+
+}
--- src/main/java/com/felipe/springcloud/msvc/oauth/services/UserService.java
+++ src/main/java/com/felipe/springcloud/msvc/oauth/services/UserService.java
@@ -1,11 +1,24 @@
 package com.felipe.springcloud.msvc.oauth.services;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import com.felipe.springcloud.msvc.oauth.models.User;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -13,9 +26,28 @@ public class UserService implements UserDetailsService {
     @Autowired
     private WebClient.Builder webClient;
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        try {
+            User user = webClient.build().get().uri("/username/{username}", params)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(User.class)
+                    .block();
+            List<GrantedAuthority> authorities = user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
+            log.info("User: " + user.toString());
+            log.info("Authorities: " + authorities);
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                    user.isEnabled(), true, true, true,
+                    authorities);
+        } catch (WebClientResponseException e) {
+            throw new UsernameNotFoundException("Error al obtener el usuario: " + username);
+        }
     }
 }
